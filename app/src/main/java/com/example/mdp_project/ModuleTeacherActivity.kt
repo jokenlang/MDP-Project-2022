45a0a5458b8947f1358225540eaf58f9e1165be3
aaronlinggo <aaronlinggosatria@gmail.com>
2022-12-31 17:09:26 +0700

    Create Module Teacher Activity

---
+++ app/src/main/java/com/example/mdp_project/ModuleTeacherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mdp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModuleTeacherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_module_teacher)
+    }
+}
