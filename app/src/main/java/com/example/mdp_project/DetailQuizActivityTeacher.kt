5f5838c33fc3d1003080781f1ed7a2bb89a113b2
Acxel Derian <acxelderian@gmail.com>
2023-01-02 13:55:42 +0700

    Create Quiz and Detail Quiz Teacher Activity

---
+++ app/src/main/java/com/example/mdp_project/DetailQuizActivityTeacher.kt
@@ -0,0 +1,11 @@
+package com.example.mdp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailQuizActivityTeacher : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_quiz_teacher)
+    }
+}
---
+++ app/src/main/java/com/example/mdp_project/QuizTeacherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mdp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizTeacherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_teacher)
+    }
+}
