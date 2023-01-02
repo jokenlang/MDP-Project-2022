d9d4d34f2249eda3f28753bd8819a702cf14fe52
aaronlinggo <aaronlinggosatria@gmail.com>
2023-01-02 20:06:42 +0700

    Update ModuleEntity.kt

--- app/src/main/java/com/example/mdp_project/ModuleEntity.kt
+++ app/src/main/java/com/example/mdp_project/ModuleEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "module")
 data class ModuleEntity (
     @PrimaryKey(autoGenerate = true)
-    var module_id: String,
+    var module_id: Int,
     var module_nama: String,
     var class_id:Int,
     var module_status:Int,
