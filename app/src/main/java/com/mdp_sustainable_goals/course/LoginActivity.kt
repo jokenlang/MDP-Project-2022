ba4fda6633a430cdc04dac2304c722a2bb003c19
Samuel Gunawan <samgun9d32@gmail.com>
2023-01-10 12:49:23 +0700

    Add remember me feature to login
    When user logged in, it will now stay that way until user explicitly press Logout button on navigation menu.

--- app/src/main/java/com/mdp_sustainable_goals/course/LoginActivity.kt
+++ app/src/main/java/com/mdp_sustainable_goals/course/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.mdp_sustainable_goals.course
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
@@ -17,6 +18,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+    companion object {
+        const val user_username = "user_username"
+        const val user_email = "user_email"
+        const val user_name = "user_name"
+        const val user_password = "user_password"
+        const val user_role = "user_role"
+        const val user_saldo = "user_saldo"
+    }
+
     lateinit var inUsernameLogin: EditText
     lateinit var inPasswordLogin: EditText
     lateinit var btnLogin: Button
@@ -47,6 +57,7 @@ class LoginActivity : AppCompatActivity() {
                 val tempUser = db.userDao().getAll()
                 user.clear()
                 user.addAll(tempUser)
+                checkSharedData()
             }
         }
 
@@ -103,8 +114,10 @@ class LoginActivity : AppCompatActivity() {
                             val intent = Intent(this, StudentActivity::class.java)
                             intent.putExtra("indexa", index)
                             intent.putExtra("username", username)
+                            intent.putExtra("nama", nama)
                             startActivity(intent)
                         }
+                        saveSharedData(user[index])
                         clearAllFields()
                         Toast(this@LoginActivity).showCustomToast(
                             "Berhasil login!",
@@ -137,18 +150,22 @@ class LoginActivity : AppCompatActivity() {
                         );
                     } else {
                         val username = user[index].username
+                        val nama = user[index].name
                         if (user[index].role == "teacher") {
                             val intent = Intent(this, TeacherActivity::class.java)
                             intent.putExtra("indexa", index)
                             intent.putExtra("username", username)
+                            intent.putExtra("nama", nama)
                             startActivity(intent)
                         } else {
                             // student
                             val intent = Intent(this, StudentActivity::class.java)
                             intent.putExtra("indexa", index)
                             intent.putExtra("username", username)
+                            intent.putExtra("nama", nama)
                             startActivity(intent)
                         }
+                        saveSharedData(user[index])
                         clearAllFields()
                         Toast(this@LoginActivity).showCustomToast(
                             "Berhasil Login!",
@@ -168,5 +185,50 @@ class LoginActivity : AppCompatActivity() {
 
     private fun checkEmail(email: String): Boolean {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun saveSharedData(userEntity: UserEntity) {
+        val sharedFile = packageName
+        val shared: SharedPreferences = getSharedPreferences(sharedFile, MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = shared.edit()
+        editor.putString(user_username, userEntity.username)
+        editor.putString(user_email, userEntity.email)
+        editor.putString(user_name, userEntity.name)
+        editor.putString(user_role, userEntity.role)
+        editor.apply()
+    }
+
+    private fun checkSharedData() {
+        val sharedFile = packageName
+        val shared: SharedPreferences = getSharedPreferences(sharedFile, MODE_PRIVATE)
+        val userRole = shared.getString(user_role, "-")
+        if (userRole != "-") {
+            val userUsername = shared.getString(user_username, "-")
+            val userNama = shared.getString(user_name, "-")
+            var checkUser = false
+            var index = 0
+            for(i in 0 until user.size) {
+                if (userUsername == user[i].username) {
+                    // ketemu kembar
+                    checkUser = true
+                    index = i
+                    break
+                }
+            }
+            if(userRole == "teacher") {
+                val intent = Intent(this, TeacherActivity::class.java)
+                intent.putExtra("indexa", index)
+                intent.putExtra("username", userUsername)
+                intent.putExtra("nama", userNama)
+                startActivity(intent)
+            } else {
+                // student
+                val intent = Intent(this, StudentActivity::class.java)
+                intent.putExtra("indexa", index)
+                intent.putExtra("username", userUsername)
+                intent.putExtra("nama", userNama)
+                startActivity(intent)
+            }
+        }
     }
 }
