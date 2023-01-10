6a988f9bc65bfb4dfb3ca68622f5fef4ffec2884
Samuel Gunawan <samgun9d32@gmail.com>
2023-01-10 13:14:08 +0700

    Add activity for payment

---
+++ app/src/main/java/com/mdp_sustainable_goals/course/payments/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.mdp_sustainable_goals.course.payments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mdp_sustainable_goals.course.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
