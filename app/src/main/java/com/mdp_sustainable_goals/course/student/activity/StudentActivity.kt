0299bd5e67554f9778fa6e0a75ab7e56c05098c7
Samuel Gunawan <samgun9d32@gmail.com>
2023-01-10 13:13:42 +0700

    Add logout functionality to student's menu

--- app/src/main/java/com/mdp_sustainable_goals/course/student/activity/StudentActivity.kt
+++ app/src/main/java/com/mdp_sustainable_goals/course/student/activity/StudentActivity.kt
@@ -1,36 +1,27 @@
 package com.mdp_sustainable_goals.course.student.activity
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mdp_sustainable_goals.course.R
 import com.mdp_sustainable_goals.course.local_storage.AppDatabase
 import com.mdp_sustainable_goals.course.local_storage.entity.ClassEntity
-import com.mdp_sustainable_goals.course.local_storage.entity.JoinClassEntity
-import com.mdp_sustainable_goals.course.local_storage.entity.UserEntity
-import com.mdp_sustainable_goals.course.student.adapter.RVJoinClassStudentAdapter
-import com.mdp_sustainable_goals.course.student.adapter.RVListClassStudentAdapter
 import com.mdp_sustainable_goals.course.student.fragment.StudentExploreFragment
 import com.mdp_sustainable_goals.course.student.fragment.StudentListClassFragment
-import com.mdp_sustainable_goals.course.teacher.adapter.ClassDashboardTeacherAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.util.ArrayList
 
 class StudentActivity : AppCompatActivity() {
-
     private lateinit var bottom_navigation: BottomNavigationView
-    private lateinit var username : String
-    private lateinit var listClass :MutableList<ClassEntity>
+    private lateinit var username: String
+    private lateinit var listClass: MutableList<ClassEntity>
     lateinit var db: AppDatabase
     val coroutine = CoroutineScope(Dispatchers.IO)
 
@@ -46,7 +37,7 @@ class StudentActivity : AppCompatActivity() {
         changeFragment(fragment)
 
         bottom_navigation.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.nav_student_explore -> {
                     var fragment = StudentExploreFragment(username)
                     changeFragment(fragment)
@@ -55,13 +46,25 @@ class StudentActivity : AppCompatActivity() {
                     var fragment = StudentListClassFragment(username)
                     changeFragment(fragment)
                 }
+                R.id.nav_student_logout -> {
+                    val sharedFile = packageName
+                    val shared: SharedPreferences? =
+                        getSharedPreferences(sharedFile, Context.MODE_PRIVATE)
+                    val editor: SharedPreferences.Editor? = shared?.edit()
+                    editor?.remove("user_username")
+                    editor?.remove("user_email")
+                    editor?.remove("user_name")
+                    editor?.remove("user_role")
+                    editor?.apply()
+                    finish()
+                }
             }
             return@setOnItemSelectedListener true
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.option_menu,menu)
+        menuInflater.inflate(R.menu.option_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -74,8 +77,7 @@ class StudentActivity : AppCompatActivity() {
         return true
     }
 
-
-    private fun changeFragment(fragment: Fragment){
+    private fun changeFragment(fragment: Fragment) {
         val bundle = Bundle()
         fragment.arguments = bundle
         val fragmentManager = supportFragmentManager.beginTransaction()
@@ -83,11 +85,9 @@ class StudentActivity : AppCompatActivity() {
         fragmentManager.commit()
     }
 
-
     private fun redirectBack() {
         val intent = Intent()
         setResult(Activity.RESULT_OK, intent)
         finish()
     }
-
 }
